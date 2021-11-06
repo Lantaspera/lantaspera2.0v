71971d32c0bcb9a02c7a040913105c751f322bce
Adrid Sadanand p <adridsadanand0001@gmail.com>
2021-11-06 23:42:48 +0530

    first updation

---
+++ src/components/Services.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Services() {
+    return (
+        <div className="services-main-div">
+           <div className="services-heading">
+               <h2 className="heading-stag">Integrated solutions designed for small business.</h2>
+           </div>
+           <div className="services-content">
+               
+           </div>
+        </div>
+    )
+}
+
+export default Services
