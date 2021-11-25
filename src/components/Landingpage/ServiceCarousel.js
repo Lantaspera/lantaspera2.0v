c1f38f7bd3a8d1f1172473ad79605d474e28e03e
Adrid Sadanand p <adridsadanand0001@gmail.com>
2021-11-26 00:22:53 +0530

    bug-4

--- src/components/Landingpage/ServiceCarousel.js
+++ src/components/Landingpage/ServiceCarousel.js
@@ -1,69 +1,150 @@
 import React from 'react'
 import "./servicecarousel.css"
-import Carousel from "react-multi-carousel";
+
 import "react-multi-carousel/lib/styles.css";
-import Image from 'react-bootstrap/Image'
+
 import { Row, Col, Container } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card'
-import LanguageIcon from '@mui/icons-material/Language';
+import { AiOutlineCodeSandbox } from "react-icons/ai";
+import { FaLaptopCode } from "react-icons/fa";
+import { GiPencilRuler } from "react-icons/gi";
+import { MdOutlineAppSettingsAlt } from "react-icons/md";
 
 
-const responsive = {
-    superLargeDesktop: {
-        // the naming can be any, depends on you.
-        breakpoint: { max: 4000, min: 3000 },
-        items: 5,
-        slidesToSlide:5
-    },
-    desktop: {
-        breakpoint: { max: 3000, min: 1024 },
-        items: 3,
-        slidesToSlide:3
-    },
-    tablet: {
-        breakpoint: { max: 1024, min: 464 },
-        items: 1
-    },
-    mobile: {
-        breakpoint: { max: 464, min: 0 },
-        items: 1
-    }
-};
+
 
 function ServiceCarousel() {
     return (
         <div>
-            
+
             <Container className="section-two">
-  <Row>
-    <Col> <h2 className="heading-sectn2">Integrated solutions designed for small business.</h2></Col>
-  </Row>
+                <Row>
+                    <Col> <h2 className="heading-sectn2">Integrated solutions designed for small business.</h2></Col>
+                </Row>
 
-  </Container>
-            {/* <Carousel draggable={false} responsive={responsive} infinite={true} autoPlay={true} arrows={false}  >
-     
+            </Container>
 
-            </Carousel> */}
+            <Container>
+                <Row xs={1} md={3}>
+                    <Col><Row>
+                        <Col>
+                            <Card className="sctn2-card-div">
 
-            <Row>
-    <Col>
-    <Card style={{ width: '18rem' }}>
-   
 
-  <Card.Body>
-  
- <Col><LanguageIcon/></Col>
-    <Card.Title className="sctn2-cardheading">Web development</Card.Title>
-    <Card.Text  className="sctn2-cardtext">
-    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-    </Card.Text>
- <Col><a className="sctn2-atag">Learn more</a></Col>
-  </Card.Body>
-</Card>
-    
-    </Col>
-  
-  </Row>
+                                <Card.Body>
+
+                                    <Col> <button className="sctn2-icon-div"> <FaLaptopCode className="sctn2-icon" size={50} /></button></Col>
+                                    <Card.Title className="sctn2-cardheading">Web development</Card.Title>
+                                    <Card.Text className="sctn2-cardtext">
+                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+                                    </Card.Text>
+                                    <Col className="sctn2-atag"><a >Learn more</a></Col>
+                                </Card.Body>
+                            </Card>
+
+                        </Col>
+
+                    </Row></Col>
+                    <Col><Row>
+                        <Col>
+                            <Card className="sctn2-card-div">
+
+
+                                <Card.Body>
+
+                                    <Col> <button className="sctn2-icon-div"> <GiPencilRuler className="sctn2-icon" size={50} /></button></Col>
+                                    <Card.Title className="sctn2-cardheading"> Ui/Ux Designing</Card.Title>
+                                    <Card.Text className="sctn2-cardtext">
+                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+                                    </Card.Text>
+                                    <Col className="sctn2-atag"><a >Learn more</a></Col>
+                                </Card.Body>
+                            </Card>
+
+                        </Col>
+
+                    </Row></Col>
+                    <Col><Row>
+                        <Col>
+                            <Card className="sctn2-card-div">
+
+
+                                <Card.Body>
+
+                                    <Col> <button className="sctn2-icon-div"> <AiOutlineCodeSandbox className="sctn2-icon" size={50} /></button></Col>
+                                    <Card.Title className="sctn2-cardheading">Web Designing</Card.Title>
+                                    <Card.Text className="sctn2-cardtext">
+                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+                                    </Card.Text>
+                                    <Col className="sctn2-atag"><a >Learn more</a></Col>
+                                </Card.Body>
+                            </Card>
+
+                        </Col>
+
+                    </Row></Col>
+                </Row>
+                <Row xs={1} md={3}>
+                    <Col><Row>
+                        <Col>
+                            <Card className="sctn2-card-div">
+
+
+                                <Card.Body>
+
+                                    <Col> <button className="sctn2-icon-div"> < MdOutlineAppSettingsAlt className="sctn2-icon" size={50} /></button></Col>
+                                    <Card.Title className="sctn2-cardheading">Application</Card.Title>
+                                    <Card.Text className="sctn2-cardtext">
+                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+                                    </Card.Text>
+                                    <Col className="sctn2-atag"><a >Learn more</a></Col>
+                                </Card.Body>
+                            </Card>
+
+                        </Col>
+
+                    </Row></Col>
+                    <Col><Row>
+                        <Col>
+                            <Card className="sctn2-card-div">
+
+
+                                <Card.Body>
+
+                                    <Col> <button className="sctn2-icon-div"> <GiPencilRuler className="sctn2-icon" size={50} /></button></Col>
+                                    <Card.Title className="sctn2-cardheading">Automation  </Card.Title>
+                                    <Card.Text className="sctn2-cardtext">
+                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+                                    </Card.Text>
+                                    <Col className="sctn2-atag"><a >Learn more</a></Col>
+                                </Card.Body>
+                            </Card>
+
+                        </Col>
+
+                    </Row></Col>
+                    <Col><Row>
+                        <Col>
+                            <Card className="sctn2-card-div">
+
+
+                                <Card.Body>
+
+                                    <Col> <button className="sctn2-icon-div"> <FaLaptopCode className="sctn2-icon" size={50} /></button></Col>
+                                    <Card.Title className="sctn2-cardheading">Web development</Card.Title>
+                                    <Card.Text className="sctn2-cardtext">
+                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+                                    </Card.Text>
+                                    <Col className="sctn2-atag"><a >Learn more</a></Col>
+                                </Card.Body>
+                            </Card>
+
+                        </Col>
+
+                    </Row></Col>
+                </Row>
+            </Container>
+
 
         </div>
     )
