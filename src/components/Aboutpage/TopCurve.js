0351149c67af1ffe45f1c271d393c424c9354d81
Adrid Sadanand p <adridsadanand0001@gmail.com>
2021-12-03 01:32:46 +0530

    bug6.5

---
+++ src/components/Aboutpage/TopCurve.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Col, Container, Row, } from 'react-bootstrap'
+import "./topcurve.css"
+
+
+function TopCurve() {
+    return (
+        
+        <div className="curved-div">
+            <Row md={2} xs={2}>
+<Col>
+<Col>
+<h5 className="abt1-heading-crv">About Us</h5></Col>
+<Col>
+<p className="abt1-tag-crv">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</p></Col>
+
+</Col>
+
+<Col><svg id="Logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 98.72"><title>asterisk</title><polygon points="0 39.79 41.42 45.16 41.42 44.69 14.94 10.27 32.2 0.35 49.71 39.56 50.17 39.56 67.44 0 84.25 9.69 57.53 44.81 57.53 45.51 100 39.79 100 59.04 57.53 53.91 57.53 54.49 84.6 88.33 66.86 98.48 49.83 59.04 49.36 59.04 31.04 98.72 14.94 88.56 41.77 54.26 41.66 53.68 0 59.04 0 39.79"/></svg></Col>
+            </Row>
+            
+           
+       
+        <svg  viewBox="0 0 1440 250"> 
+    <path fill="#fff" fill-opacity="1" d="M0,32L48,80C96,128,192,224,288,224C384,224,480,128,576,90.7C672,53,768,75,864,96C960,117,1056,139,1152,149.3C1248,160,1344,160,1392,160L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+  </svg>
+
+      </div>
+     
+    )
+}
+
+export default TopCurve
