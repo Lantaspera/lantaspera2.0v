f59ad6badd05f39e6ddc64f6c61ce69fb118ed98
Adrid Sadanand p <adridsadanand0001@gmail.com>
2021-12-07 21:03:26 +0530

    bug 7.7

--- src/components/Aboutpage/AboutTwo.js
+++ src/components/Aboutpage/AboutTwo.js
@@ -6,70 +6,70 @@ import TipsAndUpdatesOutlinedIcon from '@mui/icons-material/TipsAndUpdatesOutlin
 
 function AboutTwo() {
     return (
-     
-            <Row className="abt2-mainsctn" >
+
+        <Row className="abt2-mainsctn" >
 
 
-                <Row className="abt2-mainsctn1" >
-                    <Col><h1 className="abt2-main-heading">Why Choose Us.</h1></Col>
-                    <Row className="abt2-sctn2" xs={1} md={1}>
+            <Row className="abt2-mainsctn1" >
+            <Col><h1 className="abt2-main-heading">We Are Expert In IT <br/>Solutions Services</h1></Col>
+                <Row className="abt2-sctn2" xs={1} md={1}>
 
-                        <Col className="abt2-colone">
-                            <Col><h1 className="abt2-col-s-head"> <TipsAndUpdatesOutlinedIcon sx={{ fontSize: 40 }} /> Our Advanced Design</h1></Col>
-                            <Col><p className="abt2-col-s-tag">Lorem ipsum dolor sit ame it, consectetur adipisicing elit, sed do eiusmod te mp or incididunt ut labore.Lorem ipsum dolor sit ame it, consectetur adipisicing elit, sed do eiusmod te mp or incididunt ut labore.</p></Col>
-
-
-                        </Col>
-
-                        <Col className="abt2-colone">
-                            <Col><h1 className="abt2-col-s-head"> <TipsAndUpdatesOutlinedIcon sx={{ fontSize: 40 }} /> Our Advanced Design</h1></Col>
-                            <Col><p className="abt2-col-s-tag">Lorem ipsum dolor sit ame it, consectetur adipisicing elit, sed do eiusmod te mp or incididunt ut labore.Lorem ipsum dolor sit ame it, consectetur adipisicing elit, sed do eiusmod te mp or incididunt ut labore.</p></Col>
+                    <Col className="abt2-colone">
+                        <Col><h1 className="abt2-col-s-head"> <TipsAndUpdatesOutlinedIcon sx={{ fontSize: 40 }} /> Our Advanced Design</h1></Col>
+                        <Col><p className="abt2-col-s-tag">Lorem ipsum dolor sit ame it, consectetur adipisicing elit, sed do eiusmod te mp or incididunt ut labore.Lorem ipsum dolor sit ame it, consectetur adipisicing elit, sed do eiusmod te mp or incididunt ut labore.</p></Col>
 
 
-                        </Col>
-                        <Col className="abt2-colone">
-                            <Col><h1 className="abt2-col-s-head"> <TipsAndUpdatesOutlinedIcon sx={{ fontSize: 40 }} /> Our Advanced Design</h1></Col>
-                            <Col><p className="abt2-col-s-tag">Lorem ipsum dolor sit ame it, consectetur adipisicing elit, sed do eiusmod te mp or incididunt ut labore.Lorem ipsum dolor sit ame it, consectetur adipisicing elit, sed do eiusmod te mp or incididunt ut labore.</p></Col>
+                    </Col>
+
+                    <Col className="abt2-colone">
+                        <Col><h1 className="abt2-col-s-head"> <TipsAndUpdatesOutlinedIcon sx={{ fontSize: 40 }} /> Our Advanced Design</h1></Col>
+                        <Col><p className="abt2-col-s-tag">Lorem ipsum dolor sit ame it, consectetur adipisicing elit, sed do eiusmod te mp or incididunt ut labore.Lorem ipsum dolor sit ame it, consectetur adipisicing elit, sed do eiusmod te mp or incididunt ut labore.</p></Col>
 
 
-                        </Col>
+                    </Col>
+                    <Col className="abt2-colone">
+                        <Col><h1 className="abt2-col-s-head"> <TipsAndUpdatesOutlinedIcon sx={{ fontSize: 40 }} /> Our Advanced Design</h1></Col>
+                        <Col><p className="abt2-col-s-tag">Lorem ipsum dolor sit ame it, consectetur adipisicing elit, sed do eiusmod te mp or incididunt ut labore.Lorem ipsum dolor sit ame it, consectetur adipisicing elit, sed do eiusmod te mp or incididunt ut labore.</p></Col>
 
 
-
-               
-                   
-
-<Col className="abt2-colone">
-    <Col><h1 className="abt2-col-s-head"> <TipsAndUpdatesOutlinedIcon sx={{ fontSize: 40 }} /> Our Advanced Design</h1></Col>
-    <Col><p className="abt2-col-s-tag">Lorem ipsum dolor sit ame it, consectetur adipisicing elit, sed do eiusmod te mp or incididunt ut labore.Lorem ipsum dolor sit ame it, consectetur adipisicing elit, sed do eiusmod te mp or incididunt ut labore.</p></Col>
-
-
-</Col>
-
-<Col className="abt2-colone">
-    <Col><h1 className="abt2-col-s-head"> <TipsAndUpdatesOutlinedIcon sx={{ fontSize: 40 }} /> Our Advanced Design</h1></Col>
-    <Col><p className="abt2-col-s-tag">Lorem ipsum dolor sit ame it, consectetur adipisicing elit, sed do eiusmod te mp or incididunt ut labore.Lorem ipsum dolor sit ame it, consectetur adipisicing elit, sed do eiusmod te mp or incididunt ut labore.</p></Col>
-
-
-</Col>
-<Col className="abt2-colone">
-    <Col><h1 className="abt2-col-s-head"> <TipsAndUpdatesOutlinedIcon sx={{ fontSize: 40 }} /> Our Advanced Design</h1></Col>
-    <Col><p className="abt2-col-s-tag">Lorem ipsum dolor sit ame it, consectetur adipisicing elit, sed do eiusmod te mp or incididunt ut labore.Lorem ipsum dolor sit ame it, consectetur adipisicing elit, sed do eiusmod te mp or incididunt ut labore.</p></Col>
-
-
-</Col>
+                    </Col>
 
 
 
-</Row>
-<Row >
-    <Col className="hai">
-    </Col>
-</Row>
-                
-</Row>
-        </Row>    
-      
+
+
+
+                    <Col className="abt2-colone">
+                        <Col><h1 className="abt2-col-s-head"> <TipsAndUpdatesOutlinedIcon sx={{ fontSize: 40 }} /> Our Advanced Design</h1></Col>
+                        <Col><p className="abt2-col-s-tag">Lorem ipsum dolor sit ame it, consectetur adipisicing elit, sed do eiusmod te mp or incididunt ut labore.Lorem ipsum dolor sit ame it, consectetur adipisicing elit, sed do eiusmod te mp or incididunt ut labore.</p></Col>
+
+
+                    </Col>
+
+                    <Col className="abt2-colone">
+                        <Col><h1 className="abt2-col-s-head"> <TipsAndUpdatesOutlinedIcon sx={{ fontSize: 40 }} /> Our Advanced Design</h1></Col>
+                        <Col><p className="abt2-col-s-tag">Lorem ipsum dolor sit ame it, consectetur adipisicing elit, sed do eiusmod te mp or incididunt ut labore.Lorem ipsum dolor sit ame it, consectetur adipisicing elit, sed do eiusmod te mp or incididunt ut labore.</p></Col>
+
+
+                    </Col>
+                    <Col className="abt2-colone">
+                        <Col><h1 className="abt2-col-s-head"> <TipsAndUpdatesOutlinedIcon sx={{ fontSize: 40 }} /> Our Advanced Design</h1></Col>
+                        <Col><p className="abt2-col-s-tag">Lorem ipsum dolor sit ame it, consectetur adipisicing elit, sed do eiusmod te mp or incididunt ut labore.Lorem ipsum dolor sit ame it, consectetur adipisicing elit, sed do eiusmod te mp or incididunt ut labore.</p></Col>
+
+
+                    </Col>
+
+
+
+                </Row>
+                <Row >
+                    <Col className="hai">
+                    </Col>
+                </Row>
+
+            </Row>
+        </Row>
+
     )
 }
 
